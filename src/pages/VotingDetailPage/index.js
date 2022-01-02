1693ea6785da06f2064b0118829502ea327e733a
Mariel <marpedrod3@alum.us.es>
2022-01-02 03:42:09 -0800

    Primera versión gráfica

--- src/pages/VotingDetailPage/index.js
+++ src/pages/VotingDetailPage/index.js
@@ -1,55 +1,151 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from "react-router-dom";
-import PageTitle from 'components/PageTitle';
-import { Card, CardContent, Typography, Grid } from '@mui/material';
-import VotingService from 'domain/service/locator/voting';
+vvar myCanvas = document.getElementById("canvas");
+ 
+myCanvas.width = 300;
+ 
+myCanvas.height = 300;
+ 
+ 
+ 
+var ctx = myCanvas.getContext("2d");
 
-export default function VotingDetailPage(props) {
-  const navigate = useNavigate();
-  const params = useParams();
-
-  const [voting, setVoting] = useState(null);
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    VotingService.findById(params.votingId)
-      .then(voting => setVoting(voting))
-      .catch(err => console.error(`error getting voting with id ${params.votingId}`, err))
-      .then(() => setLoaded(true));
-    }, [])
-    
-    
-  return (<>
-    <PageTitle 
-      onBackButtonClick={ () => navigate('/') }
-      style={{ marginTop: '1rem' }}
-    >Voting detail page</PageTitle>
-
-    { (loaded && voting != null)
-      ? (
-      <Grid container spacing={2} style={{ marginTop: '1rem' }}>
-        <Grid item xs={12} md={6}>
-          <Card>
-            <CardContent>
-              <Typography sx={{ fontSize: 18 }} color="text.primary" gutterBottom>
-                { voting.name }
-              </Typography>
-              { voting.state.name }
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          CHARTS SECTION
-        </Grid>
-        <Grid item xs={12}>
-          Census and voters data
-        </Grid>
-      </Grid> )
-      : ( /** not found *******************************/
-      <Typography sx={{ fontSize: 16 }} color="text.primary" gutterBottom>
-        No se ha encontrado ninguna votación con ID { params.votingId }
-      </Typography>
-      )
-    }
-  </>);
+function drawLine(ctx, startX, startY, endX, endY){
+ 
+    ctx.beginPath();
+ 
+    ctx.moveTo(startX,startY);
+ 
+    ctx.lineTo(endX,endY);
+ 
+    ctx.stroke();
+ 
 }
+
+function drawArc(ctx, centerX, centerY, radius, startAngle, endAngle){
+ 
+    ctx.beginPath();
+ 
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+ 
+    ctx.stroke();
+ 
+}
+
+function drawPieSlice(ctx,centerX, centerY, radius, startAngle, endAngle, color ){
+ 
+    ctx.fillStyle = color;
+ 
+    ctx.beginPath();
+ 
+    ctx.moveTo(centerX,centerY);
+ 
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+ 
+    ctx.closePath();
+ 
+    ctx.fill();
+ 
+}
+
+var myVinyls = {
+ 
+    "Classical music": 10,
+ 
+    "Alternative rock": 14,
+ 
+    "Pop": 2,
+ 
+    "Jazz": 12
+ 
+};
+
+var Piechart = function(options){
+ 
+    this.options = options;
+ 
+    this.canvas = options.canvas;
+ 
+    this.ctx = this.canvas.getContext("2d");
+ 
+    this.colors = options.colors;
+ 
+ 
+ 
+    this.draw = function(){
+ 
+        var total_value = 0;
+ 
+        var color_index = 0;
+ 
+        for (var categ in this.options.data){
+ 
+            var val = this.options.data[categ];
+ 
+            total_value += val;
+ 
+        }
+ 
+ 
+ 
+        var start_angle = 0;
+ 
+        for (categ in this.options.data){
+ 
+            val = this.options.data[categ];
+ 
+            var slice_angle = 2 * Math.PI * val / total_value;
+ 
+ 
+ 
+            drawPieSlice(
+ 
+                this.ctx,
+ 
+                this.canvas.width/2,
+ 
+                this.canvas.height/2,
+ 
+                Math.min(this.canvas.width/2,this.canvas.height/2),
+ 
+                start_angle,
+ 
+                start_angle+slice_angle,
+ 
+                this.colors[color_index%this.colors.length]
+ 
+            );
+ 
+ 
+ 
+            start_angle += slice_angle;
+ 
+            color_index++;
+ 
+        }
+ 
+ 
+ 
+    }
+    
+    var myPiechart = new Piechart(
+ 
+    {
+ 
+        canvas:myCanvas,
+ 
+        data:myVinyls,
+ 
+        colors:["#fde23e","#f16e23", "#57d9ff","#937e88"]
+ 
+    }
+ 
+);
+ 
+myPiechart.draw();
+ 
+}
+
+drawLine(ctx,100,100,200,200);
+ 
+drawArc(ctx, 150,150,150, 0, Math.PI/3);
+ 
+drawPieSlice(ctx, 150,150,150, Math.PI/2, Math.PI/2 + Math.PI/4, '#ff0000');
