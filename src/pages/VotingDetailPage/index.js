e306250b1335f6adfc46f55e001ce288c632a9ea
Carolina <carcardia2@alum.us.es>
2022-01-04 08:14:30 -0800

    [#10] Actualización gráfica
    <marpedrod3>

--- src/pages/VotingDetailPage/index.js
+++ src/pages/VotingDetailPage/index.js
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from "react-router-dom";
 import PageTitle from 'components/PageTitle';
-import { Card, CardContent, Typography, Grid } from '@mui/material';
+import { Card, CardContent, Typography, Grid, Box } from '@mui/material';
 import VotingService from 'domain/service/locator/voting';
+import { DateTime } from 'luxon';
+import { Link } from 'react-router-dom';
+
+import ChartService from 'domain/service/locator/chart';
+
+import DoughnutChart from 'components/DoughnutChart';
+import TimelineChart from 'components/TimelineChart';
+import DateRangePicker from 'components/DateRangePicker';
+
+import pageStyle from './style.module.scss';
+
 
 export default function VotingDetailPage(props) {
   const navigate = useNavigate();
@@ -10,7 +21,13 @@ export default function VotingDetailPage(props) {
 
   const [voting, setVoting] = useState(null);
   const [loaded, setLoaded] = useState(false);
-
+  const [ votings, setVotings ]  = useState([]);
+  const [ votingsByStateDataset, setVotingsByStateDataset ]  = useState(null);
+  const [ activeVotingsTimeInterval, setActiveVotingsTimeInterval ] = useState([
+    DateTime.local().startOf('week').toISO(),
+    DateTime.local().endOf('week').toISO(),
+  ]);
+  const [ activeVotingsByDate, setActiveVotingsByDate ]  = useState(null);
   useEffect(() => {
     VotingService.findById(params.votingId)
       .then(voting => setVoting(voting))
@@ -38,11 +55,33 @@ export default function VotingDetailPage(props) {
             </CardContent>
           </Card>
         </Grid>
-        <Grid item xs={12} md={6}>
-          CHARTS SECTION
-        </Grid>
         <Grid item xs={12}>
           Census and voters data
+          <Grid item xs={12} md={6}>
+        { votingsByStateDataset != null && (
+          <Card>
+            <CardContent>
+              <DoughnutChart {...votingsByStateDataset} />
+            </CardContent>
+          </Card>
+        )}
+
+        { activeVotingsByDate != null && (
+          <Card style={{ marginTop: '1rem' }}>
+            <CardContent>
+              <DateRangePicker 
+                value={activeVotingsTimeInterval}
+                onChange={
+                  (newTimeInterval) => setActiveVotingsTimeInterval(newTimeInterval)
+                }
+              />
+              <Box sx={{ marginTop: '1rem' }}>
+                <TimelineChart {...activeVotingsByDate} title="Active votings timeline" />
+              </Box>
+            </CardContent>
+          </Card>
+        )}
+      </Grid>
         </Grid>
       </Grid> )
       : ( /** not found *******************************/
