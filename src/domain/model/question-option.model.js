fdcdcfdc81224f96d99c8b433cc963f7101135a0
educielan <educielan@alum.us.es>
2022-01-07 19:32:17 +0100

    [#12] Add voting details view

---
+++ src/domain/model/question-option.model.js
@@ -0,0 +1,14 @@
+
+
+export default class QuestionOption {
+
+  
+    constructor({ number,option}) {
+      this.number = number;
+      this.option = option;
+    
+    }
+  
+    
+  
+}
---
+++ src/domain/model/voting-postproc.model.js
@@ -0,0 +1,12 @@
+
+
+export default class Postproc {
+
+  
+  constructor({ votes, number, option, postproc}) {
+    this.votes = votes;
+    this.number = number;
+    this.option = option;
+    this.postproc = postproc;
+  }
+}
